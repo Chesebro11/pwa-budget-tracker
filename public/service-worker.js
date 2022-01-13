be8b5b3723f0da671021b6bd423ff550dd6af04b
unknown <chesebrojared@gmail.com>
2022-01-13 15:23:01 -0800

    service worker i think?

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,3 +6,41 @@ const FILES_TO_CACHE = [
     './js/index.js',
     './css/styles.css'
 ];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Success!!!!!!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+})
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            return request || fetch(e.request);
+        })
+    )
+});
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(CACHE_NAME);
+            })
+
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    )
+});
