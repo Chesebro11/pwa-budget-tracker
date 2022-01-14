29147a495e5c10fdae7ff6304a048c7258b0cebc
unknown <chesebrojared@gmail.com>
2022-01-13 16:15:09 -0800

    service worker fixed?? frontpage breaks when offline :(

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'BudgetTracker-version_1.0';
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION
 
 const FILES_TO_CACHE = [
     './manifest.json',
@@ -7,20 +9,26 @@ const FILES_TO_CACHE = [
     './css/styles.css'
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log('Success!!!!!!');
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-    self.skipWaiting();
-})
-
 self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
-            return request || fetch(e.request);
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
         })
     )
 });
@@ -29,7 +37,7 @@ self.addEventListener('activate', function (e) {
     e.waitUntil(
         caches.keys().then(function (keyList) {
             let cacheKeeplist = keyList.filter(function (key) {
-                return key.indexOf(CACHE_NAME);
+                return key.indexOf(APP_PREFIX);
             })
 
             cacheKeeplist.push(CACHE_NAME);
